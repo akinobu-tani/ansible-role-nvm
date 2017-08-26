5d88291d406793bbdfad8fda7cbc7535ee30c735
Akinobu Tani <akinobu.x.tani@gmail.com>
2017-08-26 16:08:21 +0900

    npmのインストールを追加

--- test/integration/default/serverspec/nvm_spec.rb
+++ test/integration/default/serverspec/nvm_spec.rb
@@ -9,3 +9,7 @@ end
 describe command('$SHELL -lc "node --version"') do
   its(:stdout) { should contain 'v6.10.2' }
 end
+
+describe command('$SHELL -lc "yarn --version"') do
+  its(:exit_status) { should eq 0 }
+end
